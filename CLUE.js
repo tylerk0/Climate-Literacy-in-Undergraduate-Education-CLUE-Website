a4693ceb94a30f3813f6ae6278967d6dc190327d
tylerk0 <tck3g@mtmail.mtsu.edu>
2025-09-21 08:59:24 -0500

    Created Base Files for Website

---
+++ CLUE.js
@@ -0,0 +1,5 @@
+/*
+	AUTHORS:     Tyler Kramer, Johnny Duong, Lilly Schmidt, Shan Sindy, Yarely Torres
+	FILE:        CLUE.js
+	DESCRIPTION: This file contains the JavaScript code for the CLUE website.
+*/
