b4aacea42258309e93547f1cab660748d2cfdfc0
tylerk0 <tck3g@mtmail.mtsu.edu>
2025-10-15 13:49:48 -0500

    Tried Merging Shan's Files (Test #1)

--- CLUE.js
+++
@@ -1,5 +0,0 @@
-/*
-	AUTHORS:     Tyler Kramer, Johnny Duong, Lilly Schmidt, Shan Sindy, Yarely Torres
-	FILE:        CLUE.js
-	DESCRIPTION: This file contains the JavaScript code for the CLUE website.
-*/
